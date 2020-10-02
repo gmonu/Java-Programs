52634e8eabfe6dd093c6038bee0e7317261bcaa7
rajugupta971012 <72241186+rajugupta971012@users.noreply.github.com>
2020-10-02 09:23:31 +0530

    Update Calculator.java
    Multiply Function implemented

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,10 @@ package hcl;
 		public int sub(int a, int b){
 	          return a-b;
 	   }
+		
+		public int mul(int a, int b){
+	          return a*b;
+	   }
 	public static void main(String args[])
 	{
 		System.out.println(add(10,20));
